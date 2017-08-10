1537ef220baf62349a12c3a4162b07318083f563
Hitarth Shah <shahhitarth9@gmail.com>
2017-08-10 11:09:17 -0700

    Update add.js
    Delete Comments

--- registration_app/js/controllers/add.js
+++ registration_app/js/controllers/add.js
@@ -1,50 +1,3 @@
-// let myApp1 = angular.module('myApp1', ['ngMaterial', 'ngMessages'])
-//   .controller('add', function($scope) {
-//     $scope.user = {   
-//       fn: '',
-//       ln: '',
-//     };
-  
-//   $scope.addRow = function(){   
-//   $scope.user.push({ 'fn':$scope.fn, 'ln': $scope.ln });
-//   $scope.fn='';
-//   $scope.ln='';
-// };
-// });
-// myApp.controller('add', function($scope){
-// $scope.user = [
-//                   // fn :'hitarth',
-//                   // ln:'shah',
-              
-
-// $scope.addRow = function(){   
-//   let counter = 0
-//   $scope.user.push({'ln': $scope.ln});
-//   $scope.fn=''
-//   $scope.ln='';  
-//   if ($scope.drop1 === $scope.ln) {
-//     $scope.fn1=''
-//     $scope.ln1=''
-//     $scope.email='';
-//     $scope.user.push({'fn1':$scope.fn1,'ln1': $scope.ln1, 'email':$scope.email});
-//   } 
-//   console.log($scope.fn)
-// };
-
-// $scope.addFrom = function(){   
-//   $scope.user.push({'fn1':$scope.fn1,'ln1': $scope.ln1, 'email':$scope.email,'drop1':$scope.drop1});
-//   $scope.fn1=''
-//   $scope.ln1=''
-//   $scope.email=''
-//   $scope.drop1 ='';
-// };
-
-
-// $scope.del = function(i){
-//     $scope.user.splice(i,1);
-//   }
-// });
-
 
 myApp.controller('add', function($scope){
 // let user = [{
@@ -87,14 +40,6 @@ $scope.addFrom = function(){
   
 };  
 
-
-
-  // user.push({
-  //       'fn1':$scope.fn1,
-  //       'ln1': $scope.ln1,
-  //        'email': $scope.email,
-  //        'drop1':$scope.drop1
-  // })
   console.log(module)
   $scope.del = function(i){
     $scope.module.splice(i,1);
@@ -115,12 +60,3 @@ $scope.addFrom = function(){
     
   }
 });
-
-  // $scope.user.push({'fn1':$scope.fn1,'ln1': $scope.ln1, 'email':$scope.email,'drop1':$scope.drop1});
-  // $scope.fn1=''
-  // $scope.ln1=''
-  // $scope.email=''
-  // $scope.drop1 ='';
-
-
-
